f9b4d5841c80c22cc7cf868b2b0d99ac321dca59
Joshua Owens <jashcache@gmail.com>
2016-05-09 11:26:59 +0900

    Electron-mocha tests added

---
+++ app/main/main_test-M.js
@@ -0,0 +1,17 @@
+/*  In Main process Tests */
+
+import React from 'react';
+import { expect } from 'chai';
+
+describe('Main', () => {
+
+  it('App wrapper is available', (done) => {
+    let renderer = createRenderer();
+    renderer.render(<App />);
+    let appWrapper = renderer.getRenderOutput();
+    // console.log(require('util').inspect(appWrapper.type, { depth: 3 }));
+
+    expect(appWrapper.type).to.equal('div');
+    done();
+  });
+});
