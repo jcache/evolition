5aaac9683cf4aea4318aafbdd80068e732d58d07
Joshua Owens <jashcache@gmail.com>
2016-05-25 13:20:24 +0900

    Test improvement

--- src/main/_tests_/main_test.js
+++ src/main/_tests_/main_test.js
@@ -3,10 +3,9 @@
 import React from 'react';
 import { expect } from 'chai';
 import {createRenderer} from 'react-addons-test-utils';
-
 import App  from '../project/app-dev';
 
-describe('Main', () => {
+describe('Main app', () => {
 
   it('App wrapper is available', (done) => {
     let renderer = createRenderer();
@@ -17,4 +16,9 @@ describe('Main', () => {
     expect(appWrapper.type).to.equal('div');
     done();
   });
+
+  it('should first load login view');
+  it('should have a login form on the initial view');
+  it('should be able to login with an email and password');
+  it('should resize window when logged in');
 });
